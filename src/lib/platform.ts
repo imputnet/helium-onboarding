66eb617ebf60c9993960e326f1616e043d1caed8
wukko <me@wukko.me>
2025-09-12 19:22:38 +0600

    lib: add platform.ts
    used for toggling platform-specific features

---
+++ src/lib/platform.ts
@@ -0,0 +1,16 @@
+const ua = navigator.userAgent.toLowerCase();
+
+const platform = {
+    is: {
+        macos: ua.includes("mac os"),
+        linux: ua.includes("linux"),
+        windows: ua.includes("windows"),
+    },
+    get name() {
+        if (this.is.linux) return "linux";
+        if (this.is.windows) return "windows";
+        return "macos";
+    },
+}
+
+export { platform };
