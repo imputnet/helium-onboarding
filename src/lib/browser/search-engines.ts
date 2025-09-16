ad1a417555a3e87ec554f4a6f0d3b5c6c6f0b0ca
wukko <me@wukko.me>
2025-09-16 14:49:49 +0600

    lib/browser/search-engines: shuffle search engines
    even if chromium didn't shuffle search engines for us (most likely due to a change upstream which we somehow didn't notice), we make sure that they're presented as shuffled in onboarding to prevent bias

--- src/lib/browser/search-engines.ts
+++ src/lib/browser/search-engines.ts
@@ -1,16 +1,27 @@
-import { readonly, writable } from "svelte/store";
 import * as cr from "../cr";
+import { readonly, writable } from "svelte/store";
 
 const browser = cr.SearchEnginesBrowserProxyImpl.getInstance();
 const _searchEngines = writable<cr.SearchEngine[]>([]);
 
+const shuffle = (array: cr.SearchEngine[]) => {
+    return array
+        .map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
+}
+
 browser.getSearchEnginesList().then(
-    engines => _searchEngines.set(engines.defaults)
+    engines => _searchEngines.set(
+        shuffle(engines.defaults)
+    )
 );
 
 cr.addWebUiListener(
     'search-engines-changed',
-    (state: cr.SearchEnginesInfo) => _searchEngines.set(state.defaults)
+    (state: cr.SearchEnginesInfo) => _searchEngines.set(
+        shuffle(state.defaults)
+    )
 );
 
 export const searchEngines = readonly(_searchEngines);
